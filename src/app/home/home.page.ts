fd350dc2f6e6129212f2f2477d9cfe21c3613c40
KarlaRamirez00 <karl.ramirez@duocuc.cl>
2024-10-25 00:36:41 -0300

    Mejoré un poquitín la vista para Crear Asignatura del profe y la alerta en caso de ingresar mal datos en el Login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { Geolocation } from '@capacitor/geolocation';
 import { Persona } from '../agregar/agregar.page';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -17,27 +18,9 @@ export class HomePage {
   passwordError: boolean = false;
   userExists: boolean = false;
   successMessage: string = ''; 
-  latitude: number = 0; 
-  longitude: number = 0; 
 
-  constructor(private firestore:AngularFirestore, private afAuth: AngularFireAuth, private router: Router) {}
-
-  ngOnInit() {
-    this.getCurrentLocation();
-  }
-
-  async getCurrentLocation() {
-    try {
-      const coordinates = await Geolocation.getCurrentPosition();
-      this.latitude = coordinates.coords.latitude;
-      this.longitude = coordinates.coords.longitude;
-      console.log('Latitud:', this.latitude, 'Longitud:', this.longitude);
-    } catch (error) {
-      console.error('Error getting location:', error);
-      alert('No se pudo obtener la ubicación. Asegúrate de que los permisos estén habilitados.');
-    }
-  }
-  
+  constructor(private firestore: AngularFirestore, private afAuth: AngularFireAuth, private router: Router, private alertController: AlertController) {}
+ 
   async register() {
     if (this.username && this.password) {
       try {
@@ -60,42 +43,44 @@ export class HomePage {
     }
   }
 
+  async presentAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
   async login() {
     if (this.username && this.password) {
       try {
         const userCredential = await this.afAuth.signInWithEmailAndPassword(this.username, this.password);
-
         const userId = userCredential.user?.uid;
 
         if (userId) {
-          try {
-            const personaDoc = await this.firestore.collection('personas').doc(userId).get().toPromise();
-        
-            if (personaDoc && personaDoc.exists) {
-              const personaData = personaDoc.data() as Persona || {}; 
-              if (personaData.esProfesor === true) {
-                this.router.navigate(['/homeProfe']);
-              } else {
-                this.router.navigate(['/login']);
-              }
+          const personaDoc = await this.firestore.collection('personas').doc(userId).get().toPromise();
+          if (personaDoc && personaDoc.exists) {
+            const personaData = personaDoc.data() as Persona || {}; 
+            if (personaData.esProfesor === true) {
+              this.router.navigate(['/homeProfe']);
             } else {
-              console.error('El documento del usuario no existe en la colección personas.');
+              this.router.navigate(['/login']);
             }
-          } catch (error) {
-            console.error('Error al obtener el documento de Firestore:', error);
+          } else {
+            console.error('El documento del usuario no existe en la colección personas.');
           }
         }
-        
-    
       } catch (error) {
         console.error('Error al iniciar sesión:', error);
-        // Aquí podrías mostrar un mensaje de error al usuario
+        await this.presentAlert('Error de autenticación', 'Correo o contraseña incorrectos. Inténtalo de nuevo.');
       }
     } else {
-      // Manejo de errores para campos vacíos
       if (!this.username) this.usernameError = true;
-      if (!this.password || this.password.length < 5) this.passwordError = true;
-    }    
-    
+      if (!this.password || this.password.length < 5) {
+        this.passwordError = true;
+        await this.presentAlert('Error en la contraseña', 'La contraseña debe tener al menos 5 caracteres.');
+      }
+    }
   }
 }
